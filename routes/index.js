b5ca88c99f62288fd7f3f32fa09dac8aa0768241
avramkjiprovski <avramkjiprovski@gmail.com>
2021-03-18 14:37:39 +0100

    attempting to build basic form and response.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+})
+.post('/', (req, res, next)=>{
+  console.log(req.body)
+  res.render('index', {title: 'Bongo'})
+})
 
 module.exports = router;
